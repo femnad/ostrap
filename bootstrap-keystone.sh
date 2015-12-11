f4f8801ee3a70c8d28b7bc58ed463eda58623343
femnad <zehemer@fastmail.fm>
2015-12-11 20:06:02 +0200

    Init

---
+++ bootstrap-keystone.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+sudo mkdir -p /etc/keystone
+config_file="/etc/keystone/keystone.conf"
+sudo mkdir -p /var/log/keystone
+sudo cp /home/femnad/z/gl/openstack/keystone/etc/keystone.conf.sample $config_file
+sudo crudini --set $config_file DEFAULT admin_token naber
+sudo crudini --set $config_file DEFAULT debug True
+sudo crudini --set $config_file DEFAULT log_dir /var/log/keystone
+sudo crudini --set $config_file database connection mysql://keystone:keystone@controller/keystone
+sudo crudini --set $config_file token keystone.token.providers.uuid.Provider
+sudo crudini --set $config_file driver keystone.token.persistence.backends.sql.Token
+sudo crudini --set $config_file driver keystone.contrib.revoke.backends.sql.Revoke
+sudo keystone-manage db_sync keystone
---
+++ install-keystone.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/z/gl/openstack/keystone/
+sudo pip install -r requirements.txt
+sudo python setup.py install
