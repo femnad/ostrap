dcad41ea715f23bf110690b163a5a598ac48cac5
femnad <zehemer@fastmail.fm>
2015-12-23 14:20:15 +0200

    Fix Ceilometer User Creation

--- init-ceilometer.sh
+++ init-ceilometer.sh
@@ -5,7 +5,7 @@ db.addUser({user: "ceilometer",
 pwd: "CEILOMETER_DBPASS",
 roles: [ "readWrite", "dbAdmin" ]})'
 source ~/y/admin-openrc.sh
-openstack user create --password ceilometer
+openstack user create ceilometer --password ceilometer
 openstack role add --project service --user ceilometer admin
 openstack service create --name ceilometer --description "Telemetry" metering
 openstack endpoint create --publicurl http://controller:8777 --internalurl http://controller:8777 --adminurl http://controller:8777 --region RegionOne metering
