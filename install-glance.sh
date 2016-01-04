97d5e707aa69107eb9b6cbe9d2d99a639bc58123
femnad <zehemer@fastmail.fm>
2016-01-04 19:26:21 +0200

    Glance Playbook Fixes and Improvements

--- install-glance.sh
+++ install-glance.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-cd ~/z/gl/openstack/glance
-sudo python2 setup.py install
-sudo pip2 install -r requirements.txt
-sudo cp etc/glance-{api,registry}.conf /etc/glance
-mysql -u boot -pborek -e 'create database glance'
-mysql -u boot -pborek -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY 'glance';"
-mysql -u boot -pborek -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY 'GLANCE_DBPASS';"
-openstack user create --password-prompt glance
-openstack role add --project service --user glance admin
-openstack service create --name glance --description "OpenStack Image service" image
-openstack endpoint create --publicurl http://controller:9292 --internalurl http://controller:9292 --adminurl http://controller:9292 --region RegionOne image
+set -e
+repo_dir="$1"
+cd "$repo_dir"
+python2 setup.py install
+pip2 install -r requirements.txt
+mkdir -p /etc/glance
+cp $repo_dir/etc/glance-{api,registry}.conf /etc/glance
