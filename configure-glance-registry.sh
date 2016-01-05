31653502d65dc1d2ebe9235a6a197f1061f6db98
femnad <zehemer@fastmail.fm>
2016-01-05 12:03:20 +0200

    Glance Installation Fixes

--- configure-glance-registry.sh
+++ configure-glance-registry.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 config_file=/etc/glance/glance-registry.conf
-sudo crudini --set $config_file database connection mysql://glance:glance@controller/glance
+crudini --set $config_file database connection mysql://glance:glance@controller/glance
 
-sudo crudini --set $config_file keystone_authtoken auth_uri http://controller:5000
-sudo crudini --set $config_file keystone_authtoken auth_url http://controller:35357
-sudo crudini --set $config_file keystone_authtoken auth_plugin password
-sudo crudini --set $config_file keystone_authtoken project_domain_id default
-sudo crudini --set $config_file keystone_authtoken user_domain_id default
-sudo crudini --set $config_file keystone_authtoken project_name service
-sudo crudini --set $config_file keystone_authtoken username glance
-sudo crudini --set $config_file keystone_authtoken password glance
+crudini --set $config_file keystone_authtoken auth_uri http://controller:5000
+crudini --set $config_file keystone_authtoken auth_url http://controller:35357
+crudini --set $config_file keystone_authtoken auth_plugin password
+crudini --set $config_file keystone_authtoken project_domain_id default
+crudini --set $config_file keystone_authtoken user_domain_id default
+crudini --set $config_file keystone_authtoken project_name service
+crudini --set $config_file keystone_authtoken username glance
+crudini --set $config_file keystone_authtoken password glance
 
-sudo crudini --set $config_file paste_deploy flavor keystone
+crudini --set $config_file paste_deploy flavor keystone
 
-sudo crudini --set $config_file DEFAULT notification_driver noop
-sudo crudini --set $config_file DEFAULT debug True
+crudini --set $config_file DEFAULT notification_driver noop
+crudini --set $config_file DEFAULT debug True
--- configure-keystone.sh
+++ configure-keystone.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
+admin_token="$1"
 config_file=/etc/keystone/keystone.conf
-crudini --set $config_file DEFAULT admin_token naber
+
+crudini --set $config_file DEFAULT admin_token $admin_token
 crudini --set $config_file DEFAULT debug True
 crudini --set $config_file DEFAULT log_dir /var/log/keystone
+
 crudini --set $config_file database connection mysql://keystone:keystone@controller/keystone
-crudini --set $config_file token keystone.token.providers.uuid.Provider
-crudini --set $config_file driver keystone.token.persistence.backends.sql.Token
-crudini --set $config_file driver keystone.contrib.revoke.backends.sql.Revoke
+
+crudini --set $config_file memcache servers localhost:11211
+
+crudini --set $config_file token provider keystone.token.providers.uuid.Provider
+crudini --set $config_file token driver keystone.token.persistence.backends.memcache.Token
+
+crudini --set $config_file revoke driver keystone.contrib.revoke.backends.sql.Revoke
+
 keystone-manage db_sync
