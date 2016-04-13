7134c16c1f8b8858f1600315b567c53729e02e68
femnad <zehemer@fastmail.fm>
2016-04-13 14:25:16 +0300

    Various Fixes for Nova Installation Playbook

--- generate-nova-config.sh
+++ generate-nova-config.sh
@@ -5,6 +5,7 @@ virtualenv="$2"
 
 cd $repo_dir
 source "$virtualenv/bin/activate"
+pip install -r test-requirements.txt
 ./tools/config/generate_sample.sh
 cp etc/nova/nova.conf.sample /etc/nova/nova.conf
 cp etc/nova/{api-paste.ini,policy.json} /etc/nova/
