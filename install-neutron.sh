3af85a84ed50a6eb9cab39413f7baafb6274b54c
femnad <zehemer@fastmail.fm>
2016-04-13 13:57:50 +0300

    Like a True Conformist, Nova Playbook Sanitifies its Config
    * Also remove unused scripts.

--- install-neutron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-repo_dir="$1"
-virtualenv_script="$2"
-
-cd "$repo_dir"
-source "$virtualenv_script"
-pip2 install -r requirements.txt
-python2 setup.py install
--- install-openstackclient.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-repo_dir="$1"
-virtualenv_dir="$2"
-cd "$1"
-source "$virtualenv_dir/bin/activate"
-pip install -r requirements.txt
-python2 setup.py install
